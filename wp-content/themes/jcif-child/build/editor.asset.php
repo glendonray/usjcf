737c05bf2b87fc0496bb972cb47a5ba739e2caad
Glendon Grapperhaus <glendonray@gmail.com>
2026-03-22 07:57:18 -0500

    Rounded corners for boxed page content, Video Testimonial thumbnail image

--- wp-content/themes/jcif-child/build/editor.asset.php
+++ wp-content/themes/jcif-child/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '9b0f5c88200bf3017f15');
+<?php return array('dependencies' => array(), 'version' => 'bc3f6047e1e051322c33');
--- wp-content/themes/jcif-child/build/frontend.asset.php
+++ wp-content/themes/jcif-child/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '52d010d9cd2253fcb06c');
+<?php return array('dependencies' => array(), 'version' => '35e428458eec409ce280');
