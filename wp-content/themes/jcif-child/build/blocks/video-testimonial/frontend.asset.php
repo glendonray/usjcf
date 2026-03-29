e6da13e3053ac20172b534747d92672d8d48e55c
Glendon Grapperhaus <glendonray@gmail.com>
2026-03-28 21:08:16 -0500

    Video Testimonial combobox on select focus video pt 2

--- wp-content/themes/jcif-child/build/blocks/video-testimonial/frontend.asset.php
+++ wp-content/themes/jcif-child/build/blocks/video-testimonial/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '21729e2a0f7d1ff7f90f');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '0ee2879cb057d87b4c60');
--- wp-content/themes/jcif-child/build/blocks/video-testimonial/frontend.js
+++ wp-content/themes/jcif-child/build/blocks/video-testimonial/frontend.js
@@ -299,6 +299,10 @@ document.addEventListener('DOMContentLoaded', function () {
       closeListbox();
       updateSelectedInfo(video);
       updateVideoPlayer(playerContainer, video);
+      playerContainer?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest'
+      });
     }
     input.addEventListener('focus', openListbox);
     input.addEventListener('click', openListbox);
